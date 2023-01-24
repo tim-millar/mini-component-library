42031bea4e8379b1048d355259b23e173ad1296d
tim-millar <heteroskedasticity99@gmail.com>
2023-01-24 23:54:45 +0000

    implement select component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,14 +4,14 @@
  * interested, but otherwise you can rely on our docs to learn its
  * API / which props it takes.
  */
-import React from 'react';
-import styled from 'styled-components';
-import { Search, AtSign, ChevronDown } from 'react-feather';
+import React from "react";
+import styled from "styled-components";
+import { Search, AtSign, ChevronDown } from "react-feather";
 
 const icons = {
   search: Search,
-  'at-sign': AtSign,
-  'chevron-down': ChevronDown,
+  "at-sign": AtSign,
+  "chevron-down": ChevronDown,
 };
 
 const Icon = ({ id, size, strokeWidth = 1, ...delegated }) => {
@@ -24,8 +24,8 @@ const Icon = ({ id, size, strokeWidth = 1, ...delegated }) => {
   return (
     <Wrapper
       style={{
-        '--size': size + 'px',
-        '--stroke-width': strokeWidth + 'px',
+        "--size": size + "px",
+        "--stroke-width": strokeWidth + "px",
       }}
       {...delegated}
     >
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const { black, gray700, transparentGray15 } = COLORS;
+
+const IconWrapper = () => <Icon id="chevron-down" size={24} />;
+
+const SelectWrapper = styled.select`
+  color: ${gray700};
+  background-color: ${transparentGray15};
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  appearance: none;
+
+  &:hover {
+    color: ${black};
+  }
+
+  &:focus {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const LayoutWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+
+  & > div {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+
+    &:hover {
+      color: ${black};
+    }
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <LayoutWrapper>
+        <IconWrapper />
+        <SelectWrapper value={value} onChange={onChange}>
+          {children}
+        </SelectWrapper>
+      </LayoutWrapper>
+    </>
   );
 };
 
