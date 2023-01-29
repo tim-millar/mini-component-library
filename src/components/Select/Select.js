8b7348a614480218b7dd2fc51f2149691c66c21b
tim-millar <heteroskedasticity99@gmail.com>
2023-01-29 11:25:10 +0000

    course instructor's solution to the select challenge

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,51 +7,66 @@ import { getDisplayedValue } from "./Select.helpers";
 
 const { black, gray700, transparentGray15 } = COLORS;
 
-const IconWrapper = () => <Icon id="chevron-down" size={24} />;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
 
-const SelectWrapper = styled.select`
-  color: ${gray700};
-  background-color: ${transparentGray15};
-  padding: 12px 52px 12px 16px;
-  border-radius: 8px;
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
   appearance: none;
+`;
 
-  &:hover {
-    color: ${black};
+const PresentationalBit = styled.div`
+  background-color: ${transparentGray15};
+  border-radius: 8px;
+  padding: 12px 16px;
+  color: ${gray700};
+  font-size: ${16 / 16}rem;
+  line-height: ${19 / 16}rem;
+  padding-right: 52px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
   }
 
-  &:focus {
-    outline: 5px auto -webkit-focus-ring-color;
+  ${NativeSelect}:hover + & {
+    color: ${black};
   }
 `;
 
-const LayoutWrapper = styled.div`
-  display: inline-block;
-  position: relative;
-
-  & > div {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-
-    &:hover {
-      color: ${black};
-    }
-  }
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
 `;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <>
-      <LayoutWrapper>
-        <IconWrapper />
-        <SelectWrapper value={value} onChange={onChange}>
-          {children}
-        </SelectWrapper>
-      </LayoutWrapper>
-    </>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
