d42b747c2b584e80c212880b07c37c78e9737012
tim-millar <heteroskedasticity99@gmail.com>
2023-02-05 13:08:29 +0000

    feat: add small variant to text input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,17 +11,22 @@ const LayoutWrapper = styled.div`
   border-bottom: 2px solid ${COLORS.black};
   padding-left: 36px;
   height: ${36 / 16}rem;
-  width: 271px;
+  width: ${({ width }) => width}px;
   font-size: ${18 / 16}rem;
   font-weight: 700;
   color: ${COLORS.gray700};
   line-height: ${21 / 16}rem;
 `;
+
 const InputWrapper = styled.input`
   height: 100%;
-  width: ${271 - 37}px;
+  width: ${({ width }) => width - 37}px;
   appearance: none;
   border: none;
+  font-size: ${18 / 16}rem;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  line-height: ${21 / 16}rem;
 
   &:focus,
   &:focus-visible {
@@ -33,6 +38,7 @@ const InputWrapper = styled.input`
     color: ${COLORS.gray500};
   }
 `;
+
 const IconWrapper = styled.div`
   position: absolute;
   left: 0;
@@ -44,15 +50,85 @@ const IconWrapper = styled.div`
   height: 24px;
 `;
 
+const LargeIconInput = ({
+  label,
+  icon,
+  width = 271,
+  size,
+  placeholder = "Search...",
+}) => (
+  <LayoutWrapper width={width}>
+    <IconWrapper>
+      <Icon id={icon} size={24} />
+    </IconWrapper>
+    <InputWrapper width={width} placeholder={placeholder} />
+  </LayoutWrapper>
+);
+
+const SmallLayoutWrapper = styled.div`
+  position: relative;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 36px;
+  height: ${28 / 16}rem;
+  width: ${({ width }) => width}px;
+  font-size: ${14 / 16}rem;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  line-height: ${16 / 16}rem;
+`;
+
+const SmallIconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  width: 18px;
+  height: 18px;
+`;
+
+const SmallInputWrapper = styled.input`
+  height: 100%;
+  width: ${({ width }) => width - 37}px;
+  appearance: none;
+  border: none;
+  font-size: ${14 / 16}rem;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  line-height: ${16 / 16}rem;
+
+  &:focus,
+  &:focus-visible {
+    outline: none;
+  }
+
+  &:placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const SmallIconInput = ({
+  label,
+  icon,
+  width = 250,
+  size,
+  placeholder = "Search...",
+}) => (
+  <SmallLayoutWrapper width={width}>
+    <SmallIconWrapper>
+      <Icon id={icon} size={18} />
+    </SmallIconWrapper>
+    <SmallInputWrapper width={width} placeholder={placeholder} />
+  </SmallLayoutWrapper>
+);
+
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
-  return (
-    <LayoutWrapper>
-      <IconWrapper>
-        <Icon id={icon} size={24} />
-      </IconWrapper>
-      <InputWrapper placeholder="Search..." />
-    </LayoutWrapper>
-  );
+  if (size === "small") {
+    return <SmallIconInput icon={icon} width={width} />;
+  }
+  return <LargeIconInput icon={icon} width={width} />;
 };
 
 export default IconInput;
